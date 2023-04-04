24cd91768fdcf8d875577190dfb0f06b9d6e6cb7
Lokesh K <88584830+lokeshk18@users.noreply.github.com>
2023-04-04 12:03:52 +0530

    landing page finished

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -6,7 +6,7 @@ function Navbar(){
        <div className="Navbar-container">
         <nav className="navbar navbar-expand-lg bg-body-tertiary">
   <div className="container-fluid">
-    <a className="navbar-brand nav-color text-success" href="#">BYEPASS MEDICALS</a>
+    <a className="navbar-brand nav-color" href="#" style={{color:"#17c272",fontFamily: "sanserif",fontSize:"25px",marginLeft:"20px"}}>BYEPASS MEDICALS</a>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
@@ -16,14 +16,14 @@ function Navbar(){
       </ul>
       <ul className="navbar-nav">
       <li className="nav-item">
-          <Link className="nav-link active" aria-current="page" to="/home">Home</Link>
+          <Link className="nav-link active" aria-current="page" to="/home" style={{color:"#c1e6d5"}}>Home</Link>
         </li>
         <li className="nav-item">
-          <Link className="nav-link" to="/login">Login</Link>
+          <Link className="nav-link" to="/login" style={{color:"#c1e6d5"}}>Login</Link>
         </li>
         
         <li className="nav-item">
-          <Link className="nav-link" to="/register">Register</Link>
+          <Link className="nav-link" to="/register" style={{color:"#c1e6d5"}}>Register</Link>
         </li>
       </ul>
     </div>

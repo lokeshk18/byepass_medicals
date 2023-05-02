8c6f4a99c375f5cbbf58c17af3113d9c3acf9671
Lokesh K <88584830+lokeshk18@users.noreply.github.com>
2023-05-02 20:56:45 +0530

    all products

--- src/Components/Products/Products.jsx
+++ src/Components/Products/Products.jsx
@@ -1,13 +1,18 @@
 import React from "react";
-import './Products.css'
+import Navbar from "../Navbar/Navbar"
+import './Products.css';
+import Footer from "../Footer/Footer";
 
 const Products = () => {
   return (
+    <div className="prod">
+        <Navbar/>
+        <br />
     <div className="container">
 
-        <div className="products">
+        <div className="row  products">
             
-            <div className="product">
+            <div className="col-4 product">
                 <div className="image">
                     <img src="https://onemg.gumlet.io/l_watermark_346,w_690,h_700/a_ignore,w_690,h_700,c_pad,q_auto,f_auto/6cb501a7eb8149bbb4e51446359d855a.jpg" alt=""/>
                 </div>
@@ -35,7 +40,7 @@ const Products = () => {
                 </div>
             </div>
                 
-            <div className="product">
+            <div className="col-4 product">
                 <div className="image">
                     <img src="https://onemg.gumlet.io/l_watermark_346,w_690,h_700/a_ignore,w_690,h_700,c_pad,q_auto,f_auto/6cb501a7eb8149bbb4e51446359d855a.jpg" alt=""/>
                 </div>
@@ -54,7 +59,7 @@ const Products = () => {
                 </div>
             </div>
             
-            <div className="product">
+            <div className="col-4 product">
                 <div className="image">
                     <img src="https://onemg.gumlet.io/l_watermark_346,w_690,h_700/a_ignore,w_690,h_700,c_pad,q_auto,f_auto/6cb501a7eb8149bbb4e51446359d855a.jpg" alt=""/>
                 </div>
@@ -72,8 +77,39 @@ const Products = () => {
                     <button> View </button>
                 </div>
             </div>
+        </div>
 
-            <div className="product">
+        <div className="row products">
+            
+            <div className="col-4 product">
+                <div className="image">
+                    <img src="https://onemg.gumlet.io/l_watermark_346,w_690,h_700/a_ignore,w_690,h_700,c_pad,q_auto,f_auto/6cb501a7eb8149bbb4e51446359d855a.jpg" alt=""/>
+                </div>
+                <div>
+                    <p> Blood Pressure Monitor Fully Automatic, Digital BP Monitor</p>
+                </div>
+                <div className="namePrice">
+                    <h6 style={{color:"gray"}}>MRP - <del>15.99</del> </h6>
+                    
+                </div>
+                <div className="namePrice">
+                    <h6 style={{color:"gray"}}>Price -11.99 </h6>
+                    
+                </div>
+                {/* <div className="stars">
+                    <i className="fa-solid fa-star"></i>
+                    <i className="fa-solid fa-star"></i>
+                    <i className="fa-solid fa-star"></i>
+                    <i className="fa-solid fa-star"></i>
+                    <i className="fa-regular fa-star"></i>
+                    <span style={{color:"gray"}}>381 ratings</span>
+                </div> */}
+                <div className="bay">
+                    <button> View </button>
+                </div>
+            </div>
+                
+            <div className="col-4 product">
                 <div className="image">
                     <img src="https://onemg.gumlet.io/l_watermark_346,w_690,h_700/a_ignore,w_690,h_700,c_pad,q_auto,f_auto/6cb501a7eb8149bbb4e51446359d855a.jpg" alt=""/>
                 </div>
@@ -91,8 +127,8 @@ const Products = () => {
                     <button> View </button>
                 </div>
             </div>
-
-            <div className="product">
+            
+            <div className="col-4 product">
                 <div className="image">
                     <img src="https://onemg.gumlet.io/l_watermark_346,w_690,h_700/a_ignore,w_690,h_700,c_pad,q_auto,f_auto/6cb501a7eb8149bbb4e51446359d855a.jpg" alt=""/>
                 </div>
@@ -110,28 +146,10 @@ const Products = () => {
                     <button> View </button>
                 </div>
             </div>
-
-            <div className="product">
-                <div className="image">
-                    <img src="https://onemg.gumlet.io/l_watermark_346,w_690,h_700/a_ignore,w_690,h_700,c_pad,q_auto,f_auto/6cb501a7eb8149bbb4e51446359d855a.jpg" alt=""/>
-                </div>
-                <div>
-                    <p> Blood Pressure Monitor Fully Automatic, Digital BP Monitor</p>
-                </div>
-                <div className="namePrice">
-                    <h6 style={{color:"gray"}}>MRP - <del>15.99</del> </h6>
-                    
-                </div>
-                <div className="namePrice">
-                    <h6 style={{color:"gray"}}>Price -11.99 </h6>
-                </div>
-                <div className="bay">
-                    <button> View </button>
-                </div>
-            </div>
-
         </div>
     </div>
+    <Footer/>
+</div>
   )
 }
 export default Products

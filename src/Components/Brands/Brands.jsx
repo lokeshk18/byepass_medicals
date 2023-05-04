04e0611c6381435f9a7006991e4652a2ee90653f
Harini853 <harini8t@gmail.com>
2023-05-04 19:20:19 +0530

    login

--- src/Components/Brands/Brands.jsx
+++ src/Components/Brands/Brands.jsx
@@ -8,7 +8,7 @@ function Brands(){
     
     <div class="product-card" >
         <div className="img-container shadow">
-          <img src="https://onemg.gumlet.io/a_ignore,w_150,h_150,c_fit,q_auto,f_auto/ea8e4d7c-3d03-4e6e-8365-85624b874c80.png" />
+          <img  className="image-brand" src="https://onemg.gumlet.io/a_ignore,w_150,h_150,c_fit,q_auto,f_auto/ea8e4d7c-3d03-4e6e-8365-85624b874c80.png" />
         </div>
         <div className="brand">
           Optinumn Nutrition
@@ -16,7 +16,7 @@ function Brands(){
       </div>
       <div class="product-card" >
         <div className="img-container shadow">
-          <img src="https://onemg.gumlet.io/a_ignore,w_150,h_150,c_fit,q_auto,f_auto/fcc05fb2-b04f-412f-9cc4-d4b07a3cb609.png" />
+          <img  className="image-brand" src="https://onemg.gumlet.io/a_ignore,w_150,h_150,c_fit,q_auto,f_auto/fcc05fb2-b04f-412f-9cc4-d4b07a3cb609.png" />
         </div>
         <div className="brand">
         Mamaeearth
@@ -24,7 +24,7 @@ function Brands(){
       </div>
       <div class="product-card" >
         <div className="img-container shadow">
-          <img src="https://onemg.gumlet.io/a_ignore,w_150,h_150,c_fit,q_auto,f_auto/d318d3d6-447f-45ca-bb6a-a81e06c84b8b.png" />
+          <img  className="image-brand" src="https://onemg.gumlet.io/a_ignore,w_150,h_150,c_fit,q_auto,f_auto/d318d3d6-447f-45ca-bb6a-a81e06c84b8b.png" />
         </div>
         <div className="brand">
          Mr.Morepen
@@ -32,7 +32,7 @@ function Brands(){
       </div>
       <div class="product-card" >
         <div className="img-container shadow">
-          <img src="https://onemg.gumlet.io/a_ignore,w_150,h_150,c_fit,q_auto,f_auto/94ec99ab-9b49-4b2f-baf9-37abfb40b37e.png" />
+          <img  className="image-brand" src="https://onemg.gumlet.io/a_ignore,w_150,h_150,c_fit,q_auto,f_auto/94ec99ab-9b49-4b2f-baf9-37abfb40b37e.png" />
         </div>
         <div className="brand">
          Hanasplat
@@ -40,7 +40,7 @@ function Brands(){
       </div>
       <div class="product-card" >
         <div className="img-container shadow">
-          <img src="https://onemg.gumlet.io/a_ignore,w_150,h_150,c_fit,q_auto,f_auto/546d21fb-6c20-4adb-b4dc-4f7ffca65e02.png" />
+          <img className="image-brand" src="https://onemg.gumlet.io/a_ignore,w_150,h_150,c_fit,q_auto,f_auto/546d21fb-6c20-4adb-b4dc-4f7ffca65e02.png" />
         </div>
         <div className="brand">
          Contour
@@ -48,7 +48,7 @@ function Brands(){
       </div>
       <div class="product-card" >
         <div className="img-container shadow">
-          <img src="https://onemg.gumlet.io/a_ignore,w_150,h_150,c_fit,q_auto,f_auto/db74c8e2-cc74-4fa9-af8b-f38424eae479.png" />
+          <img  className="image-brand" src="https://onemg.gumlet.io/a_ignore,w_150,h_150,c_fit,q_auto,f_auto/db74c8e2-cc74-4fa9-af8b-f38424eae479.png" />
         </div>
         <div className="brand">
           Dr.Willmar Schwabe
@@ -56,7 +56,7 @@ function Brands(){
       </div>
       <div class="product-card" >
         <div className="img-container shadow">
-          <img src="https://onemg.gumlet.io/a_ignore,w_150,h_150,c_fit,q_auto,f_auto/7e9ca3f3-9ac4-4b45-acda-c3402fa439da.png" />
+          <img className="image-brand" src="https://onemg.gumlet.io/a_ignore,w_150,h_150,c_fit,q_auto,f_auto/7e9ca3f3-9ac4-4b45-acda-c3402fa439da.png" />
         </div>
         <div className="brand">
           Dabur
--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -1,33 +1,44 @@
+// 
 import React from 'react'
-import './Login.scss'
+import './Login.css'
 const Login = () => {
   return (
-    
-    <div className='container login-container'>
-      <div className='row'>
-          <div className="col-md-12 col-lg-6 offset-lg-3">
-            <div className="card shadow">
-                <div className="card-body ">
-                            <form>
-                                    <div className='mb-3'>
-                                  <label className='form-label'>Email</label>
-                                    <input type="text" placeholder='Email'  className='form-control'/>
-                                    </div>
-                                    <br />
-                                    <div className='mb-3'>
-                                         <label className='form-label'>Password</label>
-                                          <input type="password" name="" id="" placeholder='Password' className='form-control' />
-                                    </div>
-                                    <div className='d-grid gap-6'>
-                                      <button className="btn ">Log In</button>
-                                    </div>     
-      </form>
-  </div>
+    <div className="container1">
+    <div className="form-box">
+      <div className="header-form">
+        <h4 className="text-primary text-center"><i className="fa fa-user-circle" style={{fontSize:"110px"}}></i></h4>
+        <div className="image">
+        </div>
+      </div>
+      <div className="body-form">
+       <form>
+          <div className="input-group mb-3">
+<div className="input-group-prepend">
+<span className="input-group-text"><i class="fa fa-user"></i></span>
 </div>
+<input type="text" className="form-control" placeholder="Username" />
+</div>
+<div className="input-group mb-3">
+<div className="input-group-prepend">
+<span className="input-group-text"><i class="fa fa-lock"></i></span>
+</div>
+<input type="text" className="form-control" placeholder="Password" />
+</div>
+<button type="button" className="btn btn-secondary btn-block btn2">LOGIN</button>
+<div className="message">
+<div><input type="checkbox" /> Remember ME</div>
+<div><a href="#">Forgot your password</a></div>
+</div>
+</form>
+        <div className="social">
+          <a href="#"><i className="fab fa-facebook"></i></a>
+          <a href="#"><i className="fab fa-twitter-square"></i></a>
+          <a href="#"><i className="fab fa-google"></i></a>
+        </div>
+      </div>
     </div>
-    </div>
-    </div>
-  
+   </div> 
   )
 }
+
 export default Login
